5d83cab7fd5855252c1c9c2ada0716611b8a35aa
chenmingjian <starvingnow@live.cn>
2019-08-06 14:01:02 +0800

    done

--- UniversalOperator.rkt
+++ UniversalOperator.rkt
@@ -4,3 +4,14 @@
   (make-rectangular
    (+ (real-part z1) (real-part z2))
    (+ (imag-part z1) (imag-part z2))))
+
+;type data
+
+(define (attach-type type contents)
+  (cons type contents))
+
+(define (type datum)
+  (car datum))
+
+(define (contents datum)
+  (cdr datum))
