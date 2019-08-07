66f384d4bb121d3c0ca393c77ce91379f636965a
chenmingjian <starvingnow@live.cn>
2019-08-07 13:56:54 +0800

    ok

--- computational_object.rkt
+++ computational_object.rkt
@@ -23,3 +23,18 @@
   (x  (lambda (a d) d)))
 
 (display (car(cons 1 2)))
+
+; assignmentm ethod
+(define (cons x y)
+  (lambda (m)
+    (m x
+       y
+       (lambda (n) (set! x n))
+       (lambda (n) (set! y n)))))
+(define (car x)
+  (x （lambda (a d sa sd) a))
+
+(define (cdr x)
+  (x （lambda (a d sa sd) d))
+
+
