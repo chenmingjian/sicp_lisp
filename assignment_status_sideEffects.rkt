7e58a13704b2573c4ea503da7859206384a52235
chenmingjian <starvingnow@live.cn>
2019-08-07 08:26:22 +0800

    done

--- assignment_status_sideEffects.rkt
+++ assignment_status_sideEffects.rkt
@@ -14,3 +14,27 @@
 (c1)
 (c1)
 (c2)
+
+; Cesaro's method to calculate pi
+
+(define (monte-carlo reials experiment)
+  (define (iter remaining passed)
+    (cond ((= remaining 0)
+           (/ passed reials))
+          ((experiment )
+           (iter (-1+ remaining)
+                 (1+ passed)))
+          (else
+           (iter (-1+ remaining)
+                 passed)))))
+
+(define rand
+  (let ((x random-init))
+    (lambda ()
+      (set! x ( rand-update x))
+      x)))
+(define (estimate-pi n)
+  (sqrt (/ 6 (monte-carlo n cesaro))))
+(define (cesaro)
+  (= (gcd (rand) (rand)) 1))
+
