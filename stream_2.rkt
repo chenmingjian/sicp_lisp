c6420219a9e6235d79fbc5f0532c93d360705840
chenmingjian <starvingnow@live.cn>
2019-08-19 18:59:24 +0800

    OK

---
+++ stream_2.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (nth-stream n s)
+  (if (= n 0)
+      (head s)
+      (nth-stream (-1+ n) (tail s))))
+(define (print-stream s)
+  (cond ((empty-stream? e) "done")
+        (else (print (head s))
+              (print-stream (tail s)))))
+
+(define (integers-from n)
+  (cons-stream
+   n
+   (integers-from (+ 1 n))))
+
+(define integers (integers-from 1))
+
+(nth-stream 20 integers)
